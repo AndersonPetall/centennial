98a6d8fdbebc1d6e23b79526bcf6b1b618baea43
AndersonPetall <104147133+AndersonPetall@users.noreply.github.com>
2022-08-14 21:02:37 -0500

    Create Models_Customer.cs

---
+++ S2/C#_CodeFromOther/WPF_Pro/Models_Customer.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02.Models {
+	public class Customer {
+		public string CustomerID { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public DateTime? DateOfBirth { get; set; }
+		public Address Address { get; set; }
+
+		public Customer() {
+			this.CustomerID = Guid.NewGuid().ToString();
+		}
+
+		public override string ToString() {
+			return $"{this.FirstName} {this.LastName}";
+		}
+	}
+}
