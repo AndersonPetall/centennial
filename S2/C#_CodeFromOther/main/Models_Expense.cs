44a95bc8bf67959bd9cd2efd25832d3ab71b9af3
AndersonPetall <104147133+AndersonPetall@users.noreply.github.com>
2022-08-16 21:38:59 -0500

    Update Models_Expense.cs

--- S2/C#_CodeFromOther/main/Models_Expense.cs
+++ S2/C#_CodeFromOther/main/Models_Expense.cs
@@ -8,12 +8,23 @@ namespace ExpenseTracker.Models
 {
     public class Expense
     {
-        public string ExpenseId { get; private set; }
+        public string ExpenseId { get; set; }
         public int Amount { get; set; }
         public DateTime Date { get; set; }
         public Record Record { get; set; }
         public Categories Category { get; set; }
         public string Description { get; set; }
+        public Expense()
+        {
+            this.ExpenseId = Guid.NewGuid().ToString();
+        }
+        public Expense(int amount,DateTime date, Record record, string description):this()
+        {
+            this.Amount = amount;
+            this.Date = date;
+            this.Record = record;
+            this.Description = description;
+        }
         public enum Categories
         {
             grocery,
@@ -22,10 +33,6 @@ namespace ExpenseTracker.Models
             transportation,
             necessities,
             other
-        }
-        public Expense()
-        {
-            this.ExpenseId = Guid.NewGuid().ToString();
         }
     }
 }
