d84b780b423cbff41885bbf98ae6d507c0928285
AndersonPetall <104147133+AndersonPetall@users.noreply.github.com>
2022-08-16 21:39:43 -0500

    Update Utilities_ExpenseService.cs

--- S2/C#_CodeFromOther/main/Utilities_ExpenseService.cs
+++ S2/C#_CodeFromOther/main/Utilities_ExpenseService.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ExpenseTracker.Models;
 using System.IO;
 using System.Xml.Serialization;
+using System.Security.Principal;
+using ExpenseTracker.Models;
+
 namespace ExpenseTracker.Utilities
 {
     public static class ExpenseService
     {
-        private static string root = "";
+        private static string root = "C:\\test1\\";
         static ExpenseService()
         {
             if (!Directory.Exists(root))
@@ -20,27 +22,69 @@ namespace ExpenseTracker.Utilities
         }
         public static void Create(Expense expense)
         {
-
+            XmlSerializer serializer = new XmlSerializer(typeof(Expense));
+            using (Stream stream = new FileStream($"{root}{expense.ExpenseId}.xml", FileMode.Create))
+            {
+                serializer.Serialize(stream, expense);
+            }
         }
         public static void Update(Expense expense)
         {
-
+            Delete(expense.ExpenseId);
+            Create(expense);
         }
-        public static void Delete(Expense expense)
+        public static void Delete(string ID)
         {
-
+            string filename = $"{root}{ID}.xml";
+            if (File.Exists(filename)) File.Delete(filename);
+            else throw new Exception($"ERROR: Tried to delete an account that does not exists ({ID}).");
+                    
         }
-        public static List<Expense> GetAll()
+        public static Expense Get(string ID)
         {
-            List<Expense> expenses = new List<Expense>();
+            string filename = $"{root}{ID}.xml";
+            XmlSerializer serializer = new XmlSerializer(typeof(Expense));
+
+            if (File.Exists(filename))
+            {
+                using (Stream stream = new FileStream(filename, FileMode.Open))
+                {
+                    Expense expense = (Expense)serializer.Deserialize(stream);
+
+                    return expense;
+                }
+            }
+            else
+            {
+                throw new Exception($"ERROR: Tried to load an account that does not exist ({ID}).");
+            }
+        }
+        //public static List<Expense> GetAll()
+        //{
+        //    List<Expense> expenses = new List<Expense>();
+        //    var file = Directory.GetFiles(root, "*.xml");
+        //    foreach(var item in file)
+        //    {
+        //        using (var stream = new FileStream(item, FileMode.Open))
+        //        {
+        //            var serializer = new XmlSerializer(typeof(Expense));
+        //            Expense oneExpense = (Expense)serializer.Deserialize(stream);
+        //            expenses.Add(oneExpense);
+        //        }
+        //    }
+        //    return expenses;
+        //}
+        public static Stack<Expense> GetAll()
+        {
+            Stack<Expense> expenses = new Stack<Expense>();
             var file = Directory.GetFiles(root, "*.xml");
-            foreach(var item in file)
+            foreach (var item in file)
             {
                 using (var stream = new FileStream(item, FileMode.Open))
                 {
                     var serializer = new XmlSerializer(typeof(Expense));
                     Expense oneExpense = (Expense)serializer.Deserialize(stream);
-                    expenses.Add(oneExpense);
+                    expenses.Push(oneExpense);
                 }
             }
             return expenses;
